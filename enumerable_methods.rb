1d24a70cc7b76ad48cadf4480402947b752047ad
Treasure Kabareebe <treasurecalm@gmail.com>
2019-11-20 13:08:05 +0300

    Add my_select method.

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -19,5 +19,12 @@ module Enumerable
     self
   end
 
+  def my_select
+    return self.to_enum(:my_select) unless block_given?
+    selected_items = []
+    self.my_each { |item| selected_items << item if yield(item) }
+    selected_items
+  end
+
 
 end
