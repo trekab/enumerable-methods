705f4bf1b21b853b51354a38cd0e51f709366c73
Treasure Kabareebe <treasurecalm@gmail.com>
2019-11-20 18:56:30 +0300

    Fix linter errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -2,7 +2,7 @@
 
 module Enumerable
   def my_each
-    return self.to_enum unless block_given?
+    return to_enum unless block_given?
 
     i = 0
     while self[i]
@@ -13,14 +13,14 @@ module Enumerable
   end
 
   def my_each_with_index
-    return self.to_enum(:my_each_with_index) unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
-    self.my_each { |e| yield(index(e), e) }
+    my_each { |e| yield(index(e), e) }
     self
   end
 
   def my_select
-    return self.to_enum(:my_select) unless block_given?
+    return to_enum(:my_select) unless block_given?
 
     selected_items = []
     self.my_each { |item| selected_items << item if yield(item) }
@@ -33,36 +33,36 @@ module Enumerable
   end
 
   def my_any?
-    self.my_each { |item| return true if yield(item) }
+    my_each { |item| return true if yield(item) }
     false
   end
 
   def my_none?
     return true unless block_given?
 
-    self.my_each { |item| return true unless yield(item) }
+    my_each { |item| return true unless yield(item) }
     false
   end
 
   def my_count(arg = nil)
     matching_items = []
 
-    if arg != nil
-      self.my_each { |item| matching_items << item if arg == item }
+    if !arg.nil?
+      my_each { |item| matching_items << item if arg == item }
     elsif block_given? == false
-      self.my_each { |item| matching_items << item }
+      my_each { |item| matching_items << item }
     else
-      self.my_each { |item| matching_items << item if yield(item) }
+      my_each { |item| matching_items << item if yield(item) }
     end
 
     matching_items.length
   end
 
   def my_map(&proc)
-    return self.to_enum(:my_map) unless block_given?
+    return to_enum(:my_map) unless block_given?
 
     mapped_array = []
-    self.my_each { |item| mapped_array << proc.call(item) }
+    my_each { |item| mapped_array << proc.call(item) }
     mapped_array
   end
 
