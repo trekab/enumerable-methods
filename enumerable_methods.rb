929a200d9e292381db919c0c0378ab747979e9b7
Treasure Kabareebe <treasurecalm@gmail.com>
2019-11-20 12:59:57 +0300

    Create my_each_with_index method.

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -8,5 +8,16 @@ module Enumerable
     self
   end
 
+  def my_each_with_index
+    return self.to_enum(:my_each_with_index) unless block_given?
+    i = 0
+    loop do
+      yield(self[i], i)
+      i += 1
+      break if i > self.length - 1
+    end
+    self
+  end
+
 
 end
