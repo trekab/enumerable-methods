f9a23a1b0a06f6a89a8c723944b34347caba100e
Treasure Kabareebe <treasurecalm@gmail.com>
2019-11-20 22:20:21 +0300

    Fix logic error in my_inject method
    Fix the plus 1 increment answer that the my_inject method outputs making it always be off by 1.

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -15,7 +15,7 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
-    my_each { |e| yield(index(e), e) }
+    my_each { |e| yield(e, index(e)) }
     self
   end
 
@@ -66,15 +66,46 @@ module Enumerable
     mapped_array
   end
 
-  def my_inject
-    sum = 1
-    my_each do |item|
-      sum = yield(sum, item)
+  def my_inject(*args)
+    sum = 0
+    i = 0
+    raise ArgumentError, "wrong number of arguments (given 3, expected 0..2)" if args.length > 2
+
+    if (args[1].is_a?(Symbol) && args[0].is_a?(Integer))
+      sum = args[0]
+      self.my_each { |item| sum = sum.method(args[1]).call(item) }
+    elsif (args.length == 0 && block_given?)
+      self.my_each do |item|
+        (i == 0) ? sum += item : sum = yield(sum, item) 
+        i += 1
+      end
+    elsif (args[0].is_a?(Integer) && block_given?)
+      sum = args[0]
+      self.my_each { |item| sum = yield(sum, item) }
+    elsif (args.length == 1 && block_given? == false)
+      if args[0].class != Symbol && args[0].class != String
+        raise TypeError, "#{args[0]} (is neither a symbol nor a string)"
+      elsif args[0].is_a?(Symbol)
+        self.my_each do |item|
+          (i == 0) ? sum += item : sum = sum.method(args[0]).call(item)
+          i += 1
+        end
+      elsif args[0].is_a?(String)
+        operators = [:+, :-, :*, :/, :==, :=~]
+        if operators.my_any? { |o| o == args[0].to_sym }
+          self.my_each do |item|
+            (i == 0) ? sum += item : sum = sum.method(args[0].to_sym).call(item)
+            i += 1
+          end
+        else
+          raise NoMethodError, "undefined method '#{args[0]}' for 1:Integer"
+        end
+      end
     end
     sum
   end
 end
 
 def multiply_els(arr)
-  arr.my_inject { |product, item| product * item }
+  arr.my_inject { |product, item| product + item }
 end
