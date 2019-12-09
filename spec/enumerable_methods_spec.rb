2df625f424c212ce96ea5f73c8dee2b3929e7294
Treasure Kabareebe <treasurecalm@gmail.com>
2019-12-09 23:51:30 +0300

    Add #my_each tests

---
+++ spec/enumerable_methods_spec.rb
@@ -0,0 +1,31 @@
+require_relative "../enumerable_methods.rb"
+
+describe(Enumerable) do
+  let(:example_array) { [10, 20, 30, 40] }
+  let(:example_range) { (1...5) }
+  let(:rand_num) {rand(5)}
+  let(:test_block) {proc { |element| element.even? }}
+
+  describe("#my_each") do
+    context("when called with a block") do
+      it("returns an array when called on array") do
+        expect(example_array.my_each { test_block }).to(eql(example_array.each { test_block }))
+      end
+
+      it("returns an range when called on a range") do
+        expect(example_range.my_each { test_block }).to(eql(example_range.each { test_block }))
+      end
+    end
+
+    context("when called without a block") do
+      it("returns an array enumerator when called on an array without a block") do
+        expect(example_array.my_each.class).to(eql(example_array.each.class))
+      end
+
+      it("returns a range enumerator when called on a range without a block") do
+        expect(example_range.my_each.class).to(eql(example_range.each.class))
+      end
+    end
+  end
+
+end
